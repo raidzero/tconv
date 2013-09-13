f4e5d77ab388c3aab0371d65812947af15105b54
raidzero <paul.raidzero@gmail.com>
2013-09-13 13:34:33 -0600

    first commit

---
+++ TConv/src/main/java/com/raidzero/tconf/Tconf.java
@@ -0,0 +1,99 @@
+package com.raidzero.tconf;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by raidzero on 9/13/13.
+ */
+public class Tconf extends Activity implements View.OnClickListener{
+    private static final String TAG = "TConf";
+
+    private static final int UNIT_F = 0;
+    private static final int UNIT_C = 1;
+    private static final int UNIT_K = 2;
+
+    EditText input_temp;
+    EditText input_unit;
+    EditText dest_unit;
+    Button convert_button;
+    TextView result_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // set up listeners
+        input_temp = (EditText) findViewById(R.id.input_temp);
+        input_temp.setOnClickListener(this);
+
+        input_unit = (EditText) findViewById(R.id.input_unit);
+        input_unit.setOnClickListener(this);
+
+        dest_unit = (EditText) findViewById(R.id.dest_unit);
+        dest_unit.setOnClickListener(this);
+
+        convert_button = (Button) findViewById(R.id.go_button);
+        convert_button.setOnClickListener(this);
+
+        result_view = (TextView) findViewById(R.id.result_view);
+    }
+
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.input_unit:
+                hideSoftKeyboard(this);
+                get_temp_unit(input_unit);
+                break;
+            case R.id.dest_unit:
+                hideSoftKeyboard(this);
+                get_temp_unit(dest_unit);
+                break;
+            case R.id.go_button:
+                hideSoftKeyboard(this);
+
+                float input_temp_value = Float.valueOf(input_temp.getText().toString());
+                String input_unit_value = input_unit.getText().toString();
+                String dest_unit_value = dest_unit.getText().toString();
+
+                char s_unit = input_unit_value.charAt(0);
+                char d_unit = dest_unit_value.charAt(0);
+                Temp t = new Temp(input_temp_value, s_unit);
+                double res = t.convert(d_unit); // do conversion using convert method on Temp object
+
+                char deg_symbol = '\u00B0';
+                result_view.setText("" + input_temp_value + " " + deg_symbol + s_unit + " -> " + d_unit + ":\n" + res + " " + deg_symbol + d_unit);
+                break;
+        }
+    }
+
+    // get the unit from a list presented to user and set the view's text to it
+    public void get_temp_unit(final EditText v)
+    {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.unit_select)
+                .setItems(R.array.temp_units,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialoginterface, int i) {
+                                String[] unitsArray = getResources().getStringArray(R.array.temp_units);
+                                v.setText(unitsArray[i]);
+                            }
+                        })
+                .show();
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+}
---
+++ TConv/src/main/java/com/raidzero/tconf/Temp.java
@@ -0,0 +1,63 @@
+package com.raidzero.tconf;
+
+/**
+ * Created by raidzero on 9/13/13.
+ */
+public class Temp {
+    private char unit;
+    private float value;
+
+    public Temp(float v, char u) {
+        this.value = v;
+        this.unit = u;
+    }
+
+    public double convert(char destUnit)
+    {
+        double res = 0;
+        switch(destUnit) {
+            case 'C':
+                switch(unit) {
+                    case 'F':
+                    res = ((value - 32) * 5) / 9.0; // convert F to C
+                    break;
+                case 'C':
+                    res = value; //already C
+                    break;
+                case 'K':
+                    res = value - 273.15; // convert K to C
+                    break;
+                }
+                break;
+            case 'K':
+                switch(unit)
+                {
+                    case 'F':
+                        res = ((value - 32) / 1.8) + 273.15; // convert F to K
+                        break;
+                    case 'C':
+                        res = value + 273.15; // convert C to K
+                        break;
+                    case 'K':
+                        res = value; // already K
+                        break;
+                }
+                break;
+            case 'F':
+                switch(unit)
+                {
+                    case 'F':
+                        res = value; // already F
+                        break;
+                    case 'C':
+                        res = ((value * 9) / 5.0) + 32;// convert C to F
+                        break;
+                    case 'K':
+                        res = ((value - 273.15) * 1.8) + 32; // convert K to F
+                        break;
+                }
+                break;
+        }
+        return res;
+    }
+}
